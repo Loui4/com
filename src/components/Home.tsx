495c33e9d4e9c3d640da9c32db0ae06d9cf998ed
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-11-23 16:30:57 +0200

    sample form

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,9 @@
 import { isPlatform } from "@ionic/react";
 import MobileHome from "../android/Home";
 import WebHome from "../web/pages/Home";
+import { SampleForm } from "./SampleForm";
 
 export const Home = () => {
-  return isPlatform("android") ? <MobileHome /> : <WebHome />;
+  // return isPlatform("android") ? <MobileHome /> : <WebHome />;
+  return <SampleForm />;
 };
